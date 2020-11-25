00adbbc21df4ea227dbb0a73259bca7194d16ca3
felipe647 <fg727505@gmail.com>
2020-11-24 23:11:26 -0500

    felipe numero 13 agregue el veu price

---
+++ src/views/Price.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="home">
+    <h1> PRICE </h1> 
+    <br>   
+    <br>   
+    <br>   
+                 <h1>GET A LOT, FOR A LITTLE</h1>
+                 <br>
+               <h1>Start whit a 30-day free trial</h1> 
+               <h1>NO CREDIT CARD, NO COMMITMENT.</h1>
+               <br>
+               <br>
+               <br>
+    <h1>If you like Kerika, ypu have three great options</h1>
+<br>
+<br>
+<br>
+<kerika-price-card plan-type="INDIVIDUAL" class="style-scope kerika-price x-scope kerika-price-card-0">
+
+    <akc-meta-query key="appConfig" class="style-scope kerika-price-card"></akc-meta-query>
+
+    <div class="layout vertical center-center text-center style-scope kerika-price-card">
+      <h4 class="plan-title dark-secondary-fg style-scope kerika-price-card">INDIVIDUAL PLAN</h4>
+      <h2 class="plan-price-info green-text style-scope kerika-price-card">Free</h2>
+      <h4 class="plan-description secondary-text style-scope kerika-price-card">For people working by themselves</h4>
+      <h4 class="plan-team-info style-scope kerika-price-card">As many Visitors as you like, but no Team Members</h4>
+      <a class="style-scope kerika-price-card" href="/en/individual-plan.html"><paper-button class="button-transparent-maroon button-small style-scope kerika-price-card x-scope paper-button-0" role="button" tabindex="0" animated="" aria-disabled="false" elevation="0">Learn more</paper-button></a>
+    </div>
+  </kerika-price-card>
+<kerika-price-card plan-type="NONPROFIT" class="style-scope kerika-price x-scope kerika-price-card-0">
+
+    <akc-meta-query key="appConfig" class="style-scope kerika-price-card"></akc-meta-query>
+
+    <div class="layout vertical center-center text-center style-scope kerika-price-card">
+      <h4 class="plan-title dark-secondary-fg style-scope kerika-price-card">ACADEMIC &amp; NONPROFIT</h4>
+      <h2 class="plan-price-info green-text style-scope kerika-price-card">Free</h2>
+      <h4 class="plan-description secondary-text style-scope kerika-price-card">For students, teachers, and small nonprofits</h4>
+      <h4 class="plan-team-info style-scope kerika-price-card">Up to 10 Team Members and unlimited Visitors</h4>
+      <a class="style-scope kerika-price-card" href="/en/nonprofit-plan.html"><paper-button class="button-transparent-maroon button-small style-scope kerika-price-card x-scope paper-button-0" role="button" tabindex="0" animated="" aria-disabled="false" elevation="0">Learn more</paper-button></a>
+    </div>
+  </kerika-price-card>
+  <kerika-price-card plan-type="PROFESSIONAL" class="style-scope kerika-price x-scope kerika-price-card-0">
+
+    <akc-meta-query key="appConfig" class="style-scope kerika-price-card"></akc-meta-query>
+
+    <div class="layout vertical center-center text-center style-scope kerika-price-card">
+      <h4 class="plan-title dark-secondary-fg style-scope kerika-price-card">PROFESSIONAL</h4>
+      <h2 class="plan-price-info green-text style-scope kerika-price-card">$7 per month</h2>
+      <h4 class="plan-description secondary-text style-scope kerika-price-card">For professionals &amp; teams in larger organizations</h4>
+      <h4 class="plan-team-info style-scope kerika-price-card">Each Team Member costs $7 Visitors are free</h4>
+      <a class="style-scope kerika-price-card" href="/en/professional-plan.html"><paper-button class="button-transparent-maroon button-small style-scope kerika-price-card x-scope paper-button-0" role="button" tabindex="0" animated="" aria-disabled="false" elevation="0">Learn more</paper-button></a>
+    </div>
+  </kerika-price-card>
+  <kerika-slide class="kerika-feature style-scope kerika-price x-scope kerika-slide-1">
+    
+    
+      <section class="main-container style-scope kerika-slide">
+        <section class="container style-scope kerika-slide">
+          
+          
+            <section class="slide-heading style-scope kerika-slide">
+              <h3 heading="" class="dark-secondary-fg style-scope kerika-price">What you get for $7</h3>
+            </section>
+          <template is="dom-if" class="style-scope kerika-slide"></template>
+          
+          
+          
+            <section class="slide-description style-scope kerika-slide">
+              
+            </section>
+          <template is="dom-if" class="style-scope kerika-slide"></template>
+        </section>
+      </section>
+    <template is="dom-if" class="style-scope kerika-slide"></template>
+    
+    
+    
+      <section class="slide-content style-scope kerika-slide">
+        <section slide-content="" class="style-scope kerika-price">
+        <section class="main-container style-scope kerika-price">
+          <section class="container style-scope kerika-price">
+            <section class="layout horizontal row-6 kerika-feature-list style-scope kerika-price">
+              <section class="col-4 style-scope kerika-price">
+                <h4 class="default-primary-fg style-scope kerika-price">Unlimited boards</h4>
+                <span class="style-scope kerika-price">Work on as many projects as you need</span>
+              </section>
+              <section class="col-4 style-scope kerika-price">
+                <h4 class="default-primary-fg style-scope kerika-price">Unlimited content</h4>
+                <span class="style-scope kerika-price">Share as much as you like</span>
+              </section>
+              <section class="col-4 style-scope kerika-price">
+                <h4 class="default-primary-fg style-scope kerika-price">Free visitors</h4>
+                <span class="style-scope kerika-price">Team members are an additional $7 per month</span>
+              </section>
+            </section>
+            <section class="layout horizontal row-6 kerika-feature-list style-scope kerika-price">
+              <section class="col-4 style-scope kerika-price">
+                <h4 class="default-primary-fg style-scope kerika-price">Custom Templates</h4>
+                <span class="style-scope kerika-price">Capture your best practices and workflows</span>
+              </section>
+              <section class="col-4 style-scope kerika-price">
+                <h4 class="default-primary-fg style-scope kerika-price">Fully functional, all the time</h4>
+                <span class="style-scope kerika-price">Always access the latest version of Kerika</span>
+              </section>
+              <section class="col-4 style-scope kerika-price">
+                <h4 class="default-primary-fg style-scope kerika-price">Priority Support</h4>
+                <span class="style-scope kerika-price">Get help by phone or email, as you prefer</span>
+              </section>
+            </section>
+            </section>
+          </section>
+        </section>
+      </section>
+    <template is="dom-if" class="style-scope kerika-slide"></template>
+  </kerika-slide>
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
